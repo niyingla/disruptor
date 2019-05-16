08e3d7c2a7514fc1a282efb46c253342764b341a
1047984367@qq.com <as1314520>
2019-05-16 09:59:27 +0800

    增加注解

--- disruptor-api/src/main/java/com/pikaqiu/disruptor/quickstart/OrderEventProducer.java
+++ disruptor-api/src/main/java/com/pikaqiu/disruptor/quickstart/OrderEventProducer.java
@@ -1,8 +1,8 @@
 package com.pikaqiu.disruptor.quickstart;
 
-import java.nio.ByteBuffer;
-
 import com.lmax.disruptor.RingBuffer;
+
+import java.nio.ByteBuffer;
 
 public class OrderEventProducer {
 
@@ -19,7 +19,7 @@ public class OrderEventProducer {
 			//2 根据这个序号, 找到具体的 "OrderEvent" 元素 注意:此时获取的OrderEvent对象是一个没有被赋值的"空对象"
 			OrderEvent event = ringBuffer.get(sequence);
 			//3 进行实际的赋值处理
-			event.setValue(data.getLong(0));			
+			event.setValue(data.getLong(0));
 		} finally {
 			//4 提交发布操作
 			ringBuffer.publish(sequence);			
