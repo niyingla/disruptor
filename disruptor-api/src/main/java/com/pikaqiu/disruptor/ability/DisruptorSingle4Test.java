ed32f1fc6e5a4a6d9d20af71bc1d3df60c42b522
huohuo <1047984367@qq.com>
2022-02-06 11:28:43 +0800

    增加注释

--- disruptor-api/src/main/java/com/pikaqiu/disruptor/ability/DisruptorSingle4Test.java
+++ disruptor-api/src/main/java/com/pikaqiu/disruptor/ability/DisruptorSingle4Test.java
@@ -16,33 +16,42 @@ public class DisruptorSingle4Test {
     public static void main(String[] args) {
         int ringBufferSize = 65536;
         final Disruptor<Data> disruptor = new Disruptor<Data>(
-                 new EventFactory<Data>() {
-					@Override
+                //创建新的存储对象工厂 用于创造对象
+                new EventFactory<Data>() {
+                    @Override
                     public Data newInstance() {
-						return new Data();
-					}
-				},
+                        return new Data();
+                    }
+                },
+                //环形队列大小
                 ringBufferSize,
+                //线程池
                 Executors.newSingleThreadExecutor(),
+                //创建一个带有单个事件发布者的
                 ProducerType.SINGLE,
                 //new BlockingWaitStrategy()
+                //阻塞等待策略
                 new YieldingWaitStrategy()
-        		);
+        );
 
         DataConsumer consumer = new DataConsumer();
         //消费数据
         disruptor.handleEventsWith(consumer);
         disruptor.start();
-        new Thread(new Runnable() {
 
+        new Thread(new Runnable() {
             @Override
             public void run() {
+                //获取环形队列
                 RingBuffer<Data> ringBuffer = disruptor.getRingBuffer();
                 for (long i = 0; i < Constants.EVENT_NUM_OHM; i++) {
                     long seq = ringBuffer.next();
+                    //获取对应序号的数据
                     Data data = ringBuffer.get(seq);
+                    //设置内容
                     data.setId(i);
                     data.setName("c" + i);
+                    //发布当前序号的数据
                     ringBuffer.publish(seq);
                 }
             }
